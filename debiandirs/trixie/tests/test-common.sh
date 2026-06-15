3e31385aaa18fdcb154121e1e80b9de1c6b7b315
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2026-06-14 20:48:32 -0400

    Debian-only packaging cleanup and changelog truncation.
    Strip Ubuntu conditionals from rules and test-common.sh, add openssl.cnf,
    truncate changelogs to trixie/unstable history, remove FAQ.html and
    stale Ubuntu doc sections.

--- debiandirs/trixie/tests/test-common.sh
+++ debiandirs/trixie/tests/test-common.sh
@@ -1,12 +1,3 @@
-
-if dpkg-vendor --derives-from Ubuntu; then
-  vendor=Ubuntu
-elif dpkg-vendor --derives-from Debian; then
-  vendor=Debian
-else
-  vendor=Unknown
-fi
-
 export LOCPATH=$(pwd)/locales
 sh $debian_dir/locale-gen
 
@@ -30,28 +21,13 @@ TESTEXCLUSIONS="$TESTEXCLUSIONS test_lib2to3"
 TESTEXCLUSIONS="$TESTEXCLUSIONS test_tcl"
 
 # FIXME: flaky/slow test?
-if [ "$vendor" = Debian ]; then
-  TESTEXCLUSIONS="$TESTEXCLUSIONS test_asyncio"
-fi
+TESTEXCLUSIONS="$TESTEXCLUSIONS test_asyncio"
 
 # FIXME: testWithTimeoutTriggeredSend: timeout not raised by _sendfile_use_sendfile
 TESTEXCLUSIONS="$TESTEXCLUSIONS test_socket"
 
-# FIXME, failing on the Ubuntu autopkg testers
-if [ "$vendor" = Ubuntu ]; then
-  TESTEXCLUSIONS="$TESTEXCLUSIONS test_code_module"
-fi
-
-# test_ssl currently assumes that OpenSSL is compiled with SECURITY_LEVEL=1
-# set security level to 1 for now, to make test_ssl pass
+# test_ssl assumes OpenSSL SECLEVEL=1
 export OPENSSL_CONF=$debian_dir/openssl.cnf
-
-# FIXME: Fails with Ubuntu's autopkg test infrastructure
-if [ "$vendor" = Ubuntu ]; then
-  if [ "$(dpkg --print-architecture)" = arm64 ]; then
-    TESTEXCLUSIONS="$TESTEXCLUSIONS test_io"
-  fi
-fi
 
 # FIXME: test_ttk_guionly times out on many buildds
 TESTEXCLUSIONS="$TESTEXCLUSIONS test_ttk_guionly"
@@ -61,3 +37,4 @@ TESTEXCLUSIONS="$TESTEXCLUSIONS test_ttk_textonly"
 
 # FIXME: test_multiprocessing_fork times out sometimes. See #1000188
 TESTEXCLUSIONS="$TESTEXCLUSIONS test_multiprocessing_fork"
+
